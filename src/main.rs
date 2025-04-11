66227b1a8bcdd763e2a0a479bd024e76aec0d365
qamosu <bahorin.ivan@gmail.com>
2025-04-11 19:13:06 +0200

    fixed the baseline

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ use std::collections::HashMap;
 
 use dashmap::DashMap;
 use log::debug;
-use nrs_language_server::completion::completion;
-use nrs_language_server::nrs_lang::{
+use mel_language_server::completion::ImCompleteCompletionItem;
+use mel_language_server::completion::completion;
+use mel_language_server::nrs_lang::{
     parse, type_inference, Ast, ImCompleteSemanticToken, ParserResult,
 };
-use nrs_language_server::semantic_analyze::{analyze_program, IdentType, Semantic};
-use nrs_language_server::semantic_token::LEGEND_TYPE;
-use nrs_language_server::span::Span;
+use mel_language_server::semantic_analyze::{analyze_program, IdentType, Semantic};
+use mel_language_server::semantic_token::LEGEND_TYPE;
+use mel_language_server::span::Span;
 use ropey::Rope;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
@@ -315,10 +316,10 @@ impl LanguageServer for Backend {
                     k.start,
                     k.end,
                     match v {
-                        nrs_language_server::nrs_lang::Value::Null => "null".to_string(),
-                        nrs_language_server::nrs_lang::Value::Bool(_) => "bool".to_string(),
-                        nrs_language_server::nrs_lang::Value::Num(_) => "number".to_string(),
-                        nrs_language_server::nrs_lang::Value::Str(_) => "string".to_string(),
+                        mel_language_server::nrs_lang::Value::Null => "null".to_string(),
+                        mel_language_server::nrs_lang::Value::Bool(_) => "bool".to_string(),
+                        mel_language_server::nrs_lang::Value::Num(_) => "number".to_string(),
+                        mel_language_server::nrs_lang::Value::Str(_) => "string".to_string(),
                     },
                 )
             })
@@ -365,7 +366,7 @@ impl LanguageServer for Backend {
             let mut ret = Vec::with_capacity(completions.len());
             for (_, item) in completions {
                 match item {
-                    nrs_language_server::completion::ImCompleteCompletionItem::Variable(var) => {
+                    ImCompleteCompletionItem::Variable(var) => {
                         ret.push(CompletionItem {
                             label: var.clone(),
                             insert_text: Some(var.clone()),
@@ -374,7 +375,7 @@ impl LanguageServer for Backend {
                             ..Default::default()
                         });
                     }
-                    nrs_language_server::completion::ImCompleteCompletionItem::Function(
+                    ImCompleteCompletionItem::Function(
                         name,
                         args,
                     ) => {
